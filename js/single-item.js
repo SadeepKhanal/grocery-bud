e83f58399a24794bae81fa6500d18c472991a9f6
Sadeep Khanal <sadeepkhanal343@gmail.com>
2026-01-31 14:11:42 +0545

    Implement remove item functionality in createSingleItem
    Added functionality to remove an item when the remove button is clicked . It simply passes the control to app.js to remove the item

--- js/single-item.js
+++ js/single-item.js
@@ -1,4 +1,4 @@
-import { editCompleted } from "./app.js";
+import { editCompleted,removeItem } from "./app.js";
 export function createSingleItem(item) {
   const div = document.createElement("div");
   div.className = "single-item";
@@ -15,10 +15,14 @@ export function createSingleItem(item) {
       <i class="fa-regular fa-trash-can"></i>
     </button>
   `;
-  const checkbox = div.querySelector('input[type="checkbox"]');
+   const checkbox = div.querySelector('input[type="checkbox"]');
    checkbox.addEventListener("change", () => editCompleted(item.id));
+
+  const removeBtn = div.querySelector(".remove-btn");
+  removeBtn.addEventListener("click", () => removeItem(item.id));
 
   return div;
 
 }
+
 
